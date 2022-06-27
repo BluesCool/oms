bdf3d4ed24142e7b13118b8b72d62b411a0e142d
BluesCool <for584920@gmail.com>
2022-06-27 21:52:54 +0800

    创建第一次

---
+++ oms-common/src/main/java/com/ydgj/oms/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,11 @@
+package com.ydgj.oms.common.core.domain.model;
+
+/**
+ * 用户注册对象
+ * 
+ * @author ruoyi
+ */
+public class RegisterBody extends LoginBody
+{
+
+}
---
+++ oms-quartz/src/main/java/com/ydgj/oms/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.ydgj.oms.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.ydgj.oms.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
